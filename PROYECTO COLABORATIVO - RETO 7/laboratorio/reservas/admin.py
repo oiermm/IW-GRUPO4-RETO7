15630f62cf1337887555e0229611b7c71189d23a
oiermm <oier.martinez@opendeusto.es>
2026-04-22 15:29:40 +0200

    Delete PROYECTO COLABORATIVO - RETO 7 directory

--- PROYECTO COLABORATIVO - RETO 7/laboratorio/reservas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Usuario, Recurso, Reserva
-
-# Register your models here.
-
-admin.site.register(Usuario)
-admin.site.register(Recurso)
-admin.site.register(Reserva)
